5951358708e5107f3072d7d733b2da5b131c723a
Аристотел <95682278+a0ris0to0te0l@users.noreply.github.com>
2024-01-12 17:34:55 +0100

    Uploads

---
+++ devilspie2.lua
@@ -0,0 +1,42 @@
+if (get_application_name() == 'LibreOffice 7.4' and get_window_name():find('Office')) then
+  set_window_workspace(1);
+end
+if (get_application_name() == 'Firefox' and get_window_name():find('Firefox')) then
+  set_window_workspace(2);
+end
+if (get_application_name() == 'Clementine' and get_window_name():find('Firefox')) then
+  set_window_workspace(2);
+end
+if (get_application_name() == 'Sublime Text' and get_window_name():find('Sublime')) then
+  set_window_workspace(3);
+end
+if (get_application_name() == 'nemo' and get_window_name():find('Files')) then
+  set_window_workspace(4);
+end
+if (get_application_name() == 'psppire' and get_window_name():find('Data')) then
+  set_window_workspace(5);
+end
+if (get_application_name() == 'gretl_x11' and get_window_name():find('Data')) then
+  set_window_workspace(5);
+end
+if (get_application_name() == 'rstudio' and get_window_name():find('Data')) then
+  set_window_workspace(5);
+end
+if (get_application_name() == 'GnuCash' and get_window_name():find('Data')) then
+  set_window_workspace(5);
+end
+if (get_application_name() == 'Document Viewer' and get_window_name():find('PDF')) then
+  set_window_workspace(6);
+end
+if (get_application_name() == 'Thunderbird' and get_window_name():find('Thunderbird')) then
+  set_window_workspace(7);
+end
+if (get_application_name() == 'Telegram Desktop' and get_window_name():find('RSS')) then
+  set_window_workspace(8);
+end
+if (get_application_name() == 'QuiteRSS' and get_window_name():find('RSS')) then
+  set_window_workspace(8);
+end
+if (get_application_name() == 'Time Log' and get_window_name():find('Time')) then
+  set_window_workspace(9);
+end
